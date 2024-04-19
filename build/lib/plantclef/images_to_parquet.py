8d61ca602d45aa518d03eb132b26ddbdb238e428
Ian Stalter <istalter@securonix.com>
2024-04-19 07:03:28 -0700

    added to git ignore

---
+++ build/lib/plantclef/images_to_parquet.py
@@ -0,0 +1,132 @@
+import argparse
+from pathlib import Path
+
+from pyspark.sql import functions as F
+
+from plantclef.utils import get_spark
+
+"""
+Before running this script, make sure you have downloaded and extracted the dataset into the data folder.
+Use the bash file `download_extract_dataset.sh` in the scripts folder.
+"""
+
+
+def create_dataframe(spark, base_dir: Path, raw_root_path: str, meta_dataset_name: str):
+    """Converts images into binary data and joins with a Metadata DataFrame"""
+    # Load all files from the base directory as binary data
+    image_df = (
+        spark.read.format("binaryFile")
+        .option("pathGlobFilter", "*.jpg")
+        .option("recursiveFileLookup", "true")
+        .load(base_dir.as_posix())
+    )
+
+    # Construct the string to be replaced - adjust this based on your actual base path
+    to_remove = "file:" + str(base_dir.parents[0])
+
+    # Remove "file:{base_dir.parents[0]" from path column
+    image_df = image_df.withColumn("path", F.regexp_replace("path", to_remove, ""))
+
+    # Split the path into an array of elements
+    split_path = F.split(image_df["path"], "/")
+
+    # Select and rename columns to fit the target schema, including renaming 'content' to 'data'
+    image_final_df = image_df.select(
+        "path",
+        F.element_at(split_path, -1).alias("image_name"),
+        F.col("content").alias("data"),
+    )
+
+    # Read the iNaturalist metadata CSV file
+    meta_df = spark.read.csv(
+        f"{raw_root_path}/{meta_dataset_name}.csv",
+        header=True,
+        inferSchema=True,
+        sep=";",  # specify semicolon as delimiter
+    )
+
+    # Drop duplicate entries based on 'image_path' before the join
+    meta_final_df = meta_df.dropDuplicates(["image_name"])
+
+    # Perform an inner join on the 'image_path' column
+    final_df = image_final_df.join(meta_final_df, "image_name", "inner").repartition(
+        500, "species_id"
+    )
+
+    return final_df
+
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Process images and metadata for a dataset stored on GCS."
+    )
+    parser.add_argument(
+        "--cores", type=int, default=4, help="Number of cores used in Spark driver"
+    )
+    parser.add_argument(
+        "--memory",
+        type=str,
+        default="8g",
+        help="Amount of memory to use in Spark driver",
+    )
+    parser.add_argument(
+        "--image-root-path",
+        type=str,
+        default=str(Path(".").resolve()),
+        help="Base directory path for image data",
+    )
+    parser.add_argument(
+        "--raw-root-path",
+        type=str,
+        default="gs://dsgt-clef-plantclef-2024/raw/",
+        help="Root directory path for metadata",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        default="gs://dsgt-clef-plantclef-2024/data/parquet_files/PlantCLEF2024_training",
+        help="GCS path for output Parquet files",
+    )
+    parser.add_argument(
+        "--dataset-name",
+        type=str,
+        default="PlantCLEF2024",
+        help="Dataset name downloaded from tar file",
+    )
+    parser.add_argument(
+        "--meta-dataset-name",
+        type=str,
+        default="PlantCLEF2024singleplanttrainingdata",
+        help="Train Metadata CSV file",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main function that processes data and writes the output dataframe to GCS"""
+    args = parse_args()
+
+    # Initialize Spark with settings for using the big-disk-dev VM
+    spark = get_spark(
+        cores=args.cores, memory=args.memory, **{"spark.sql.shuffle.partitions": 500}
+    )
+
+    # Convert raw-root-path to a Path object here
+    base_dir = Path(args.image_root_path) / "data" / args.dataset_name
+
+    # Create image dataframe
+    final_df = create_dataframe(
+        spark=spark,
+        base_dir=base_dir,
+        raw_root_path=args.raw_root_path,
+        meta_dataset_name=args.meta_dataset_name,
+    )
+
+    # Write the DataFrame to GCS in Parquet format
+    final_df.write.mode("overwrite").parquet(args.output_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ build/lib/plantclef/test.py
@@ -0,0 +1,93 @@
+import os
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+
+def preprocess_image(image_path, target_size=(224, 224)):
+    if os.path.exists(image_path):
+        img = load_img(image_path, target_size=target_size)
+        img_array = img_to_array(img) / 255.0
+        return img_array
+    else:
+        print(f"Image not found: {image_path}")
+        return None
+
+
+# Load CSV file
+data = pd.read_csv("PlantCLEF2022_web_training_metadata.csv", delimiter=";")
+
+
+# List IDs in the directory
+directory_ids = os.listdir("/Users/istalter/Desktop/images")
+directory_ids = [
+    int(directory_id) for directory_id in directory_ids if directory_id.isdigit()
+]
+
+num_classes_csv = data["species"].nunique()
+
+# Get number of unique directory IDs
+num_classes_dirs = len(directory_ids)
+
+# Assign the maximum of the two values to NUM_CLASSES
+NUM_CLASSES = max(num_classes_csv, num_classes_dirs)
+
+# Filter data based on directory IDs
+filtered_data = data[data["classid"].isin(directory_ids)]
+
+# Filter out None values and preprocess images
+X = []
+for img_path in filtered_data["image_path"]:
+    img_array = preprocess_image(img_path)
+    if img_array is not None:
+        X.append(img_array)
+    else:
+        print(f"Failed to preprocess image: {img_path}")
+
+# Convert list to numpy array
+X = np.array(X)
+
+# Print the number of images processed
+print("Number of images processed:", len(X))
+
+# Assuming you have labels and want to train a model
+
+# Load labels
+y = filtered_data["species"]
+
+# Encode labels
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Define model
+model = models.Sequential(
+    [
+        layers.Flatten(input_shape=(224, 224, 3)),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(NUM_CLASSES, activation="softmax"),
+    ]
+)
+
+# Compile model
+model.compile(
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
+
+# Train model
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+# Evaluate model
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Test Loss:", test_loss)
+print("Test Accuracy:", test_accuracy)
