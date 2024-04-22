efd82f9d1c5ff17bcaf900c2535ec320846159aa
Murilo Gustineli <murilogustineli@gmail.com>
2024-04-22 13:21:55 +0000

    added process test data logic into luigi pipeline

--- plantclef/workflows/baseline.py
+++ plantclef/workflows/baseline.py
@@ -18,9 +18,10 @@ class ProcessBase(luigi.Task):
     input_path = luigi.Parameter()
     output_path = luigi.Parameter()
     should_subset = luigi.BoolParameter(default=False)
-    num_partitions = luigi.IntParameter(default=500)
+    sample_col = luigi.Parameter(default="species_id")
+    num_partitions = luigi.OptionalIntParameter(default=500)
     sample_id = luigi.OptionalIntParameter(default=None)
-    num_sample_id = luigi.IntParameter(default=10)
+    num_sample_id = luigi.OptionalIntParameter(default=10)
 
     def output(self):
         if self.sample_id is None:
@@ -45,7 +46,7 @@ class ProcessBase(luigi.Task):
             transformed = (
                 transformed.withColumn(
                     "sample_id",
-                    F.crc32(F.col("species_id").cast("string")) % self.num_sample_id,
+                    F.crc32(F.col(self.sample_col).cast("string")) % self.num_sample_id,
                 )
                 .where(F.col("sample_id") == self.sample_id)
                 .drop("sample_id")
@@ -93,47 +94,54 @@ class ProcessBase(luigi.Task):
 
 
 class ProcessDino(ProcessBase):
+    sql_statement = luigi.Parameter()
+
     @property
-    def feature_columns(self):
+    def feature_columns(self) -> list:
         return ["dino_embedding"]
 
     def pipeline(self):
         dino = WrappedDinoV2(input_col="data", output_col="dino_embedding")
-        return Pipeline(
-            stages=[
-                dino,
-                SQLTransformer(
-                    statement=f"SELECT image_name, species_id, dino_embedding FROM __THIS__"
-                ),
-            ]
-        )
+        return Pipeline(stages=[dino, SQLTransformer(statement=self.sql_statement)])
 
 
 class ProcessDCT(ProcessBase):
+    sql_statement = luigi.Parameter()
+
     @property
-    def feature_columns(self):
+    def feature_columns(self) -> list:
         return ["dct_embedding"]
 
     def pipeline(self):
         dct = DCTN(input_col="dino_embedding", output_col="dct_embedding")
-        return Pipeline(
-            stages=[
-                dct,
-                SQLTransformer(
-                    statement=f"SELECT image_name, species_id, dct_embedding FROM __THIS__"
-                ),
-            ]
-        )
+        return Pipeline(stages=[dct, SQLTransformer(statement=self.sql_statement)])
 
 
 class Workflow(luigi.Task):
     input_path = luigi.Parameter()
     output_path = luigi.Parameter()
     default_root_dir = luigi.Parameter()
+    process_test_data = luigi.OptionalBoolParameter(default=False)
 
     def run(self):
+        # training workflow parameters
+        subset_list = [True, False]
+        train_model = True
+        sample_col = "species_id"
+        dino_sql_statement = (
+            "SELECT image_name, species_id, dino_embedding FROM __THIS__"
+        )
+        dct_sql_statement = "SELECT image_name, species_id, dct_embedding FROM __THIS__"
+        # test workflow parameters
+        if self.process_test_data:
+            subset_list = [False]  # Skip subset for test data
+            train_model = False  # Skip model training
+            sample_col = "image_name"
+            dino_sql_statement = "SELECT image_name, dino_embedding FROM __THIS__"
+            dct_sql_statement = "SELECT image_name, dct_embedding FROM __THIS__"
+
         # Run jobs with subset and full-size data
-        for subset in [True, False]:
+        for subset in subset_list:
             final_output_path = self.output_path
             if subset:
                 subset_path = f"subset_{self.output_path.split('/')[-1]}"
@@ -148,6 +156,8 @@ class Workflow(luigi.Task):
                     should_subset=subset,
                     sample_id=i,
                     num_sample_id=10,
+                    sample_col=sample_col,
+                    sql_statement=dino_sql_statement,
                 )
                 for i in range(10)
             ]
@@ -155,21 +165,24 @@ class Workflow(luigi.Task):
                 input_path=f"{final_output_path}/dino/data",
                 output_path=f"{final_output_path}/dino_dct",
                 should_subset=subset,
+                sample_col=sample_col,
+                sql_statement=dct_sql_statement,
             )
 
         # Train classifier outside of the subset loop
-        for limit_species in [5, None]:
-            final_default_dir = self.default_root_dir
-            if limit_species:
-                final_default_dir = (
-                    f"{self.default_root_dir}-limit-species-{limit_species}"
+        if train_model:
+            for limit_species in [5, None]:
+                final_default_dir = self.default_root_dir
+                if limit_species:
+                    final_default_dir = (
+                        f"{self.default_root_dir}-limit-species-{limit_species}"
+                    )
+                yield TrainDCTEmbeddingClassifier(
+                    input_path=f"{self.output_path}/dino_dct/data",
+                    feature_col="dct_embedding",
+                    default_root_dir=final_default_dir,
+                    limit_species=limit_species,
                 )
-            yield TrainDCTEmbeddingClassifier(
-                input_path=f"{self.output_path}/dino_dct/data",
-                feature_col="dct_embedding",
-                default_root_dir=final_default_dir,
-                limit_species=limit_species,
-            )
 
 
 def parse_args():
@@ -198,15 +211,27 @@ def parse_args():
         default="models/torch-petastorm-v1",
         help="Default root directory for storing the pytorch classifier runs",
     )
+    parser.add_argument(
+        "--process-test-data",
+        type=bool,
+        default=False,
+        help="If True, set pipeline to process the test data and extract embeddings",
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    # Input and output paths
+    # Input and output paths for training workflow
     input_path = f"{args.gcs_root_path}/{args.train_data_path}"
     output_path = f"{args.gcs_root_path}/{args.output_name_path}"
     default_root_dir = f"{args.gcs_root_path}/{args.model_dir_path}"
+    process_test_data = args.process_test_data
+
+    # update workflow parameters for processing test data
+    if process_test_data:
+        input_path = f"{args.gcs_root_path}/data/parquet_files/PlantCLEF2024_test"
+        output_path = f"{args.gcs_root_path}/data/process/test_v1"
 
     luigi.build(
         [
@@ -214,6 +239,7 @@ if __name__ == "__main__":
                 input_path=input_path,
                 output_path=output_path,
                 default_root_dir=default_root_dir,
+                process_test_data=process_test_data,
             )
         ],
         scheduler_host="services.us-central1-a.c.dsgt-clef-2024.internal",
