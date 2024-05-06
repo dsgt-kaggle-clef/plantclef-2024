3e2b3a828a381f2ee46e64d2413d7a113b5e2c42
Murilo Gustineli <murilogustineli@gmail.com>
2024-05-06 04:26:23 +0000

    updated pretrained dino class, needs to fix bytes-like object not int

--- plantclef/transforms.py
+++ plantclef/transforms.py
@@ -1,6 +1,7 @@
 import io
 
 import numpy as np
+import pandas as pd
 import timm
 import torch
 from PIL import Image
@@ -10,7 +11,7 @@ from pyspark.ml.param.shared import HasInputCol, HasOutputCol
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
-from pyspark.sql.types import ArrayType, FloatType
+from pyspark.sql.types import ArrayType, FloatType, MapType, StringType
 from scipy.fftpack import dctn
 from transformers import AutoImageProcessor, AutoModel
 
@@ -30,8 +31,8 @@ class WrappedDinoV2(
         self,
         input_col: str = "input",
         output_col: str = "output",
-        model_name="facebook/dinov2-base",
-        batch_size=8,
+        model_name: str = "facebook/dinov2-base",
+        batch_size: int = 8,
     ):
         super().__init__()
         self._setDefault(inputCol=input_col, outputCol=output_col)
@@ -138,9 +139,9 @@ class ExtractCLSToken(
 
     def __init__(
         self,
-        input_col="dino_embedding",
-        output_col="cls_embedding",
-        token_dimension=768,
+        input_col: str = "dino_embedding",
+        output_col: str = "cls_embedding",
+        token_dimension: int = 768,
     ):
         super(ExtractCLSToken, self).__init__()
         self.input_col = input_col
@@ -170,8 +171,8 @@ class PretrainedDinoV2(
         pretrained_path: str,
         input_col: str = "input",
         output_col: str = "output",
-        model_name="vit_base_patch14_reg4_dinov2.lvd142m",
-        batch_size=8,
+        model_name: str = "vit_base_patch14_reg4_dinov2.lvd142m",
+        batch_size: int = 8,
     ):
         super().__init__()
         self._setDefault(inputCol=input_col, outputCol=output_col)
@@ -179,9 +180,10 @@ class PretrainedDinoV2(
         self.batch_size = batch_size
         self.pretrained_path = pretrained_path
         self.num_classes = 7806  # total number of plant species
+        self.local_directory = "/mnt/data/models/pretrained_models"
+        self.class_mapping_file = f"{self.local_directory}/class_mapping.txt"
         # Model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.processor = AutoImageProcessor.from_pretrained(self.model_name)
         self.model = timm.create_model(
             self.model_name,
             pretrained=False,
@@ -189,38 +191,67 @@ class PretrainedDinoV2(
             checkpoint_path=self.pretrained_path,
         )
         self.model.to(self.device)
+        self.model.eval()
+        # Data transform
+        self.data_config = timm.data.resolve_model_data_config(self.model)
+        self.transforms = timm.data.create_transform(
+            **self.data_config, is_training=False
+        )
+
+    def _load_class_mapping(self):
+        with open(self.class_mapping_file) as f:
+            class_index_to_class_name = {i: line.strip() for i, line in enumerate(f)}
+        return class_index_to_class_name
 
     def _make_predict_fn(self):
         """Return PredictBatchFunction using a closure over the model"""
+        self.cid_to_spid = self._load_class_mapping()
 
         def predict(inputs: np.ndarray) -> np.ndarray:
             images = [Image.open(io.BytesIO(input)) for input in inputs]
-            model_inputs = self.processor(images=images, return_tensors="pt").to(
-                self.device
-            )
-            # # Move inputs to device
-            # model_inputs = {
-            #     key: value.to(self.device) for key, value in model_inputs.items()
-            # }
+            processed_images = [self.transforms(image).unsqueeze(0) for image in images]
+            batch_input = torch.cat(processed_images).to(self.device)
 
             with torch.no_grad():
-                outputs = self.model(**model_inputs)
-                last_hidden_states = outputs.last_hidden_state
+                outputs = self.model(batch_input)
+                # convert logits to probabilities and scale them
+                probabilities = torch.softmax(outputs, dim=1) * 100
+                # get top 20 probabilities
+                top_probs, top_indices = torch.topk(probabilities, k=20)
 
-            numpy_array = last_hidden_states.cpu().numpy()
-            new_shape = numpy_array.shape[:-2] + (-1,)
-            numpy_array = numpy_array.reshape(new_shape)
+            top_probs = top_probs.cpu().numpy()
+            top_indices = top_indices.cpu().numpy()
+            print(f"\ntop_probs: {top_probs}")
+            print(f"top_indices: {top_indices}\n")
 
-            return numpy_array
+            # Convert top indices and probabilities to a dictionary {species_id: probability}
+            result = []
+            for probs, indices in zip(top_probs, top_indices):
+                result.append(
+                    {
+                        self.cid_to_spid[index]: float(prob)
+                        for index, prob in zip(indices, probs)
+                    }
+                )
+            print(f"result: {result}")
+            return result
 
         return predict
 
-    def _transform(self, df: DataFrame):
-        return df.withColumn(
-            self.getOutputCol(),
-            predict_batch_udf(
-                make_predict_fn=self._make_predict_fn,
-                return_type=ArrayType(FloatType()),
-                batch_size=self.batch_size,
-            )(self.getInputCol()),
+    def _transform(self, df):
+        predict_udf = F.udf(
+            self._make_predict_fn(),
+            ArrayType(MapType(StringType(), FloatType())),
         )
+
+        return df.withColumn(self.getOutputCol(), predict_udf(self.getInputCol()))
+
+    # def _transform(self, df: DataFrame):
+    #     return df.withColumn(
+    #         self.getOutputCol(),
+    #         predict_batch_udf(
+    #             make_predict_fn=self._make_predict_fn,
+    #             return_type=ArrayType(MapType(StringType(), FloatType())),
+    #             batch_size=self.batch_size,
+    #         )(self.getInputCol()),
+    #     )
